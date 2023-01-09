1c018e4f3736b5a638bc47df1d4b22623de099e8
ItsFelixRodriguez <ItsFelixRodriguez@gmail.com>
2023-01-09 14:59:01 +0900

    first pass at obj constructor

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ function hello() {
 	alert('Hello World!')
 }
 
-
-
 let myLibrary = [];
 
-function Book() {
-	//constuctor
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
+const theHobbit = new Book ('The Hobbit', 'J.R.R. Tolkien', 295, 'read')
+
 
 function addBookToLibrary () {
 	//do stuff onclick event
